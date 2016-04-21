a31da14507438a0754317fa8f8feba596ed56204
Brbb <fdl@MacBook-Air-di-Francesco.local>
2016-04-21 19:16:23 +0200

    update

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
+// An example for OpenShift platform.
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '208697960:AAFmqkbcQkb2iLk41E40XUpr4jEUzCyZlgw';
+// See https://developers.openshift.com/en/node-js-environment-variables.html
+var port = process.env.OPENSHIFT_NODEJS_PORT;
+var host = process.env.OPENSHIFT_NODEJS_IP;
+var domain = process.env.OPENSHIFT_APP_DNS;
 
-// Setup polling way
-var bot = new TelegramBot(token, {polling: true});
-
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
-});
-
-// Any kind of message
+var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
+bot.setWebHook(domain+':443/bot'+token);
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+  bot.sendMessage(chatId, "I'm alive!");
 });
