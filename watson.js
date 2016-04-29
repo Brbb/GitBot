0dd49a44dde624edf728961c0ce117104cab4f7f
Brbb <fdl@MacBook-Air-di-Francesco.local>
2016-04-29 17:13:08 +0200

    voice test

--- watson.js
+++ watson.js
@@ -78,6 +78,7 @@ exports.recognize = function (voiceFile) {
                             var writeStream = fs.createWriteStream('resources/output/'+outputVoiceFileName+'.wav');
                             // Pipe the synthesized text to a file
                             text_to_speech.synthesize(tts_params).pipe(writeStream);
+                            console.log('Produced output file.');
                             
                         }
                     });
