1ef431e6c756cd9548e658ad3fe176600e507eb8
Brbb <franxster@gmail.com>
2016-04-30 19:50:16 +0200

    Update watson.js

--- watson.js
+++ watson.js
@@ -2,20 +2,20 @@ var watson = require('watson-developer-cloud');
 var fs = require('fs');
 
 var speech_to_text = watson.speech_to_text({
-    username: '5151834d-f4b1-43eb-bd0a-7ff4b553b516',
-    password: 'vHQE5LSYpc75',
+    username: '<username>',
+    password: '<password>',
     version: 'v1'
 });
 
 var text_to_speech = watson.text_to_speech({
-    username: 'e582b8c4-3b6c-40d8-8307-83c2ba233586',
-    password: 'OMiI5gtW776I',
+    username: '<username>',
+    password: '<password>',
     version: 'v1'
 });
 
 var language_translation = watson.language_translation({
-    username: 'd6a57b5f-910a-4fe4-b6ab-7833bbfb3132',
-    password: 'vBbPekmJ6VEH',
+    username: '<username>',
+    password: '<password>',
     version: 'v2'
 });
 
